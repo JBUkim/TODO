bce4255d90a934fa29695d2df620de5c4ca6ecef
JBUkim <91812103@jmail.ac.kr>
2022-11-12 22:02:44 +0900

    5. Final

--- core/models.py
+++ core/models.py
@@ -26,3 +26,17 @@ class Post(models.Model):
 
     def str(self):
         return self.user
+
+class LikePost(models.Model) :
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def str(self):
+        return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    
+    def str(self):
+        return self.user
